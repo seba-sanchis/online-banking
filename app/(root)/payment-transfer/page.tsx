d076a0259d974c776b68170a1e202275adeb65c5
seba-sanchis <seba.sanchis@gmail.com>
2024-05-06 15:31:31 -0300

    transfer payment page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <div>payment-transfer</div>;
+import HeaderBox from "@/components/HeaderBox";
+import PaymentTransferForm from "@/components/PaymentTransferForm";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+export default async function Page() {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+
+  return (
+    <div className="payment-transfer">
+      <HeaderBox
+        title="Payment transfer"
+        subtext="Please provide any specific details or notes related to the payment transfer."
+      />
+
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={accountsData} />
+      </section>
+    </div>
+  );
 }
